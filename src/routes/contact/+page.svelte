2bc1bcd99a09fa5a437969a88e8d52909efdfe48
Adan Ayaz <adanayaztracer@gmail.com>
2023-10-29 18:17:43 +0500

    Fixed routes

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>Contact</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+  <h1 class="text-4xl font-bold underline">Contact Page</h1>
+</div>
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+  <title>Privacy Page</title>
+  <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+  <h1 class="text-4xl font-bold underline">Privacy</h1>
+</div>
